35945dd9596353c0c1a1400d26d066f64705cc05
Levidevtech <79833570+Levidevtech@users.noreply.github.com>
2021-03-26 09:50:45 +0100

    Added a simple menu
    Simple menu to test choices

--- Greenhouse/Program.cs
+++ Greenhouse/Program.cs
@@ -6,8 +6,35 @@ namespace Greenhouse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
-            Console.WriteLine("Levi");
+            bool quit = false;
+            do
+            {
+                Console.WriteLine("Press 1 for test");
+                Console.WriteLine("Press 2 for test2");
+                int choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Food1");
+                        Console.WriteLine("Food2");
+                        Console.WriteLine("Do you wanna close the program?\nType yes to close, everything else will go back to the menu");
+                        string back = Console.ReadLine();
+                        quit = back == "yes" ? true : false;
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.WriteLine("Food4");
+                        Console.WriteLine("Food5");
+                        Console.WriteLine("Food6");
+                        break;
+                }
+
+            } while (!quit);
+
+
+
         }
     }
 }
